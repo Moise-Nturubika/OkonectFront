246f63f5c2cd479de2d142a429f3d33df4d8559e
Moise-Nturubika <nturumoussa@gmail.com>
2022-03-18 10:09:39 +0200

    Add download manager library

--- lib/ui/home/homeScreen.dart
+++ lib/ui/home/homeScreen.dart
@@ -462,37 +462,40 @@ class _HomeScreenState extends State<HomeScreen> {
                     return Container(
                       height: 200,
                       child: ListView(
+                          physics: BouncingScrollPhysics(),
                           children: List.generate(
-                        state.data.length,
-                        (index) => listTileRecentAdd(
-                            title: state.data[index].category.designation
-                                        .toString()
-                                        .toLowerCase()
-                                        .contains('music') ||
-                                    state.data[index].category.designation
-                                        .toString()
-                                        .toLowerCase()
-                                        .contains('video')
-                                ? "${state.data[index].auteur} : ${state.data[index].title}"
-                                : state.data[index].title,
-                            category: state.data[index].category.designation
-                                .toString(),
-                            onPressed: () {
-                              if (state.data[index].category.designation
-                                  .toString()
-                                  .toLowerCase()
-                                  .contains('music')) {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (ctx) => AudioScreen(
-                                          media: state.data[index],
-                                        )));
-                              } else {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (ctx) =>
-                                        VideoScreen(media: state.data[index])));
-                              }
-                            }),
-                      )
+                            state.data.length,
+                            (index) => listTileRecentAdd(
+                                title: state.data[index].category.designation
+                                            .toString()
+                                            .toLowerCase()
+                                            .contains('music') ||
+                                        state.data[index].category.designation
+                                            .toString()
+                                            .toLowerCase()
+                                            .contains('video')
+                                    ? "${state.data[index].auteur} : ${state.data[index].title}"
+                                    : state.data[index].title,
+                                category: state.data[index].category.designation
+                                    .toString(),
+                                onPressed: () {
+                                  if (state.data[index].category.designation
+                                      .toString()
+                                      .toLowerCase()
+                                      .contains('music')) {
+                                    Navigator.of(context)
+                                        .push(MaterialPageRoute(
+                                            builder: (ctx) => AudioScreen(
+                                                  media: state.data[index],
+                                                )));
+                                  } else {
+                                    Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                            builder: (ctx) => VideoScreen(
+                                                media: state.data[index])));
+                                  }
+                                }),
+                          )
                           // [
                           //   listTileRecentAdd(
                           //       category: 'Film',
